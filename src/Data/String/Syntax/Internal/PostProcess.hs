1c837fec8e3e393fab8aaa7db8ce2930fabcb873
Brandon Chinn <brandonchinn178@gmail.com>
2023-01-19 20:50:30 -0800

    Implement interpolated strings

--- src/Data/String/Syntax/Internal/PostProcess.hs
+++ src/Data/String/Syntax/Internal/PostProcess.hs
@@ -5,8 +5,12 @@ module Data.String.Syntax.Internal.PostProcess (
   postProcessFile,
 ) where
 
+import Data.Bifunctor (first)
+import Data.Foldable (toList)
+import Data.List (intercalate)
 import qualified Data.List.NonEmpty as NonEmpty
 import Data.Maybe (fromMaybe)
+import qualified Data.Sequence as Seq
 import Data.Text (Text)
 import qualified Data.Text as Text
 
@@ -55,7 +59,7 @@ postProcessMultiline =
       InterpolatedStringLiteral chunks ->
         InterpolatedStringLiteral $
           case chunks of
-            (RawStringChunk x : xs) -> RawStringChunk (f x) : xs
+            RawStringChunk x : xs -> RawStringChunk (f x) : xs
             _ -> chunks
 
 removeCommonWhitespacePrefix :: StringLiteral -> StringLiteral
@@ -70,8 +74,14 @@ removeCommonWhitespacePrefix = \case
         . map (rmSpace commonPrefix)
         $ strLines
   InterpolatedStringLiteral chunks ->
-    -- TODO
-    InterpolatedStringLiteral chunks
+    let
+      chunkLines = breakInterpolatedLines chunks
+      commonPrefix = getCommonPrefix $ map fst chunkLines
+     in
+      InterpolatedStringLiteral
+        . concatInterpolatedLines
+        . map (first $ rmSpace commonPrefix)
+        $ chunkLines
   where
     isIndent c = c == ' ' || c == '\t'
 
@@ -92,6 +102,51 @@ removeCommonWhitespacePrefix = \case
           | n >= 8 -> rmSpace (n - 8) s'
           | otherwise -> Text.replicate (8 - n) " " <> s'
         _ -> s
+
+-- | Break the given chunks into a list of lines, where each line is
+-- represented as a pair containing the leading RawStringChunk and the chunks
+-- making up the rest of the line.
+breakInterpolatedLines :: [InterpolatedStringChunk] -> [(Text, [InterpolatedStringChunk])]
+breakInterpolatedLines = map splitPrefix . splitLines Seq.empty
+  where
+    splitLines currLine = \case
+      [] -> [toList currLine]
+      chunk : rest ->
+        case chunk of
+          RawStringChunk s | l : ls <- Text.splitOn "\n" s ->
+            let
+              (midLines, lastLine) = splitLast (map RawStringChunk ls)
+              thisLine = toList (currLine Seq.|> RawStringChunk l)
+              midLineChunks = map (: []) midLines
+             in thisLine : midLineChunks ++ splitLines (Seq.fromList lastLine) rest
+          _ -> splitLines (currLine Seq.|> chunk) rest
+
+    splitPrefix =
+      let fromRaw = \case
+            RawStringChunk s -> Just s
+            InterpolatedStringChunk{} -> Nothing
+       in first Text.concat . spanMaybe fromRaw
+
+    splitLast xs =
+      case NonEmpty.nonEmpty xs of
+        Nothing -> ([], [])
+        Just xs' -> (NonEmpty.init xs', [NonEmpty.last xs'])
+
+    spanMaybe :: (a -> Maybe b) -> [a] -> ([b], [a])
+    spanMaybe f = \case
+      a : as | Just b <- f a -> first (b :) $ spanMaybe f as
+      as -> ([], as)
+
+-- | The inverse of breakInterpolatedLines
+concatInterpolatedLines :: [(Text, [InterpolatedStringChunk])] -> [InterpolatedStringChunk]
+concatInterpolatedLines = mergeRawStringChunks . intercalate [RawStringChunk "\n"] . map mergePrefix
+  where
+    mergePrefix (prefix, chunks) = RawStringChunk prefix : chunks
+
+    mergeRawStringChunks = \case
+      RawStringChunk a : RawStringChunk b : rest -> mergeRawStringChunks $ RawStringChunk (a <> b) : rest
+      chunk : rest -> chunk : mergeRawStringChunks rest
+      [] -> []
 
 convertStringLiteral :: StringLiteral -> Text
 convertStringLiteral = \case
